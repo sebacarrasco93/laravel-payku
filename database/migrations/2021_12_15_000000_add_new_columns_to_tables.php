21444cf44c67041b687eb5ec4d67311e6515af54
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2021-12-15 13:29:48 -0300

    Add new columns (full_name, authorization_code, payment_key, transaction_key, deposit_date)

---
+++ database/migrations/2021_12_15_000000_add_new_columns_to_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnsToTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payku_transactions', function (Blueprint $table) {
+            $table->string('full_name')->nullable();
+        });
+
+        Schema::table('payku_payments', function (Blueprint $table) {
+            $table->string('authorization_code')->nullable();
+            $table->string('payment_key')->nullable();
+            $table->string('transaction_key')->nullable();
+            $table->date('deposit_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payku_payments');
+    }
+}
